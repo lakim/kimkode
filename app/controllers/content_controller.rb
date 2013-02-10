bfe8907e12571461d153742e6f28ff9ffdd69cb0
Louis-Alban KIM <louis.alban.kim@gmail.com>
2013-02-10 22:56:31 +0100

    Add caching todo

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -3,7 +3,7 @@ class ContentController < ApplicationController
   def show
     content = Content.find(params[:slug])
     raise ActionController::RoutingError if content.nil?
-    # Set instance variable using the proper name @page or @post
+    # Set instance variable using the proper name: @page or @post
     instance_variable_set("@#{content.class.name.underscore}", content)
     @body = process_body(content)
     render "#{content.class.name.underscore.pluralize}/show", :layout => content.layout
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,7 @@ class HomeController < ApplicationController
 
   def index
     @posts = Post.all.last(20)
+    # TODO: add caching
+    # Cache key: last_updated_file_date
   end
 end
