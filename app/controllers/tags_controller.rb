e1974270f3d22f204c0c623dc5889eb384b4e4be
Louis-Alban KIM <louis.alban.kim@gmail.com>
2013-05-06 15:13:25 +0200

    Fix tag page

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,8 +1,8 @@
 class TagsController < ApplicationController
 
   def show
-    tag = Tag.find(params[:id])
-    @posts = tag.posts
+    @tag = Tag.find(params[:id])
+    @posts = @tag.posts
     # TODO: Add caching
     # Cache key: ?
   end
